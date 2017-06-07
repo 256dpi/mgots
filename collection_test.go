c828c3e9ca965147ff626285214dc6d392197634
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-06-07 15:01:41 +0200

    updated tests

--- collection_test.go
+++ collection_test.go
@@ -8,14 +8,45 @@ import (
 )
 
 func TestCollectionInsert(t *testing.T) {
-	coll := C(db.C("test1"), Second)
+	dbc := db.C("test-coll-insert")
+	tsc := C(dbc, Second)
 
 	now := time.Now()
 
-	err := coll.Insert("test", 10.0, now, nil)
+	err := tsc.Insert("test", 10.0, now, nil)
 	assert.NoError(t, err)
 
-	ts, err := coll.Fetch("test", now.Add(-1*time.Second), now.Add(1*time.Second), nil)
+	ts, err := tsc.Fetch("test", now.Add(-1*time.Second), now.Add(1*time.Second), nil)
 	assert.NoError(t, err)
+	assert.Equal(t, now.Truncate(time.Second), ts.Points[0].Timestamp)
+	assert.Equal(t, Second, ts.Points[0].Resolution)
 	assert.Equal(t, 10.0, ts.Points[0].Value)
+	assert.Equal(t, 10.0, ts.Points[0].Max)
+	assert.Equal(t, 10.0, ts.Points[0].Min)
+	assert.Equal(t, 1, ts.Points[0].Num)
+	assert.Equal(t, 10.0, ts.Points[0].Total)
+}
+
+func TestCollectionAdd(t *testing.T) {
+	dbc := db.C("test-coll-add")
+	tsc := C(dbc, Second)
+
+	now := time.Now()
+
+	bulk := dbc.Bulk()
+
+	tsc.Add(bulk, "test", 10.0, now, nil)
+
+	_, err := bulk.Run()
+	assert.NoError(t, err)
+
+	ts, err := tsc.Fetch("test", now.Add(-1*time.Second), now.Add(1*time.Second), nil)
+	assert.NoError(t, err)
+	assert.Equal(t, now.Truncate(time.Second), ts.Points[0].Timestamp)
+	assert.Equal(t, Second, ts.Points[0].Resolution)
+	assert.Equal(t, 10.0, ts.Points[0].Value)
+	assert.Equal(t, 10.0, ts.Points[0].Max)
+	assert.Equal(t, 10.0, ts.Points[0].Min)
+	assert.Equal(t, 1, ts.Points[0].Num)
+	assert.Equal(t, 10.0, ts.Points[0].Total)
 }
