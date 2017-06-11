7404b1d17e34b0366960ee989ba97546a509b311
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-06-11 22:05:58 +0200

    added more benchmarks

--- bench_test.go
+++ bench_test.go
@@ -8,7 +8,7 @@ import (
 func BenchmarkCollectionInsert(b *testing.B) {
 	b.ReportAllocs()
 
-	coll := Wrap(db.C("bench1"), OneMinuteOf60Seconds)
+	coll := Wrap(db.C("bench-coll-insert"), OneMinuteOf60Seconds)
 
 	err := coll.EnsureIndexes(0)
 	if err != nil {
@@ -30,7 +30,7 @@ func BenchmarkCollectionInsert(b *testing.B) {
 func BenchmarkCollectionBulkInsert(b *testing.B) {
 	b.ReportAllocs()
 
-	coll := Wrap(db.C("bench2"), OneMinuteOf60Seconds)
+	coll := Wrap(db.C("bench-coll-bulk-insert"), OneMinuteOf60Seconds)
 
 	err := coll.EnsureIndexes(0)
 	if err != nil {
@@ -53,6 +53,124 @@ func BenchmarkCollectionBulkInsert(b *testing.B) {
 			}
 
 			bulk = coll.Bulk()
+		}
+	}
+}
+
+func BenchmarkCollectionBulkInsert1000(b *testing.B) {
+	b.ReportAllocs()
+
+	coll := Wrap(db.C("bench-coll-bulk-insert-1000"), OneMinuteOf60Seconds)
+
+	err := coll.EnsureIndexes(0)
+	if err != nil {
+		panic(err)
+	}
+
+	bulk := coll.Bulk()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1000; j++ {
+			bulk.Insert(time.Now(), map[string]float64{
+				"value": float64(j),
+			}, nil)
+		}
+
+		err := bulk.Run()
+		if err != nil {
+			panic(err)
+		}
+
+		bulk = coll.Bulk()
+	}
+}
+
+func BenchmarkCollectionAggregateSamples(b *testing.B) {
+	b.ReportAllocs()
+
+	coll := Wrap(db.C("bench-coll-aggregate-samples"), OneMinuteOf60Seconds)
+
+	err := coll.EnsureIndexes(0)
+	if err != nil {
+		panic(err)
+	}
+
+	bulk := coll.Bulk()
+
+	now := time.Now()
+
+	for i := 0; i < 3600; i++ {
+		bulk.Insert(now.Add(time.Duration(i)*time.Second), map[string]float64{
+			"value": float64(i),
+		}, nil)
+
+		if i%200 == 0 {
+			err := bulk.Run()
+			if err != nil {
+				panic(err)
+			}
+
+			bulk = coll.Bulk()
+		}
+	}
+
+	err = bulk.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := coll.AggregateSamples(now, now.Add(1*time.Hour), []string{"value"}, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkCollectionAggregateSets(b *testing.B) {
+	b.ReportAllocs()
+
+	coll := Wrap(db.C("bench-coll-aggregate-sets"), OneMinuteOf60Seconds)
+
+	err := coll.EnsureIndexes(0)
+	if err != nil {
+		panic(err)
+	}
+
+	bulk := coll.Bulk()
+
+	now := time.Now()
+
+	for i := 0; i < 3600; i++ {
+		bulk.Insert(now.Add(time.Duration(i)*time.Second), map[string]float64{
+			"value": float64(i),
+		}, nil)
+
+		if i%200 == 0 {
+			err := bulk.Run()
+			if err != nil {
+				panic(err)
+			}
+
+			bulk = coll.Bulk()
+		}
+	}
+
+	err = bulk.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := coll.AggregateSets(now, now.Add(1*time.Hour), []string{"value"}, nil)
+		if err != nil {
+			panic(err)
 		}
 	}
 }
