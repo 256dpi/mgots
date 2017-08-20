a1639bff2836e91cb06ce3d605742c05d206721a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-08-20 13:35:01 +0200

    make resolution panic safe

--- resolution.go
+++ resolution.go
@@ -48,22 +48,31 @@ const (
 
 // Split will return the beginning of a set and the key of the sample.
 func (r BasicResolution) Split(t time.Time) (time.Time, string) {
+	ts := time.Time{}
+	key := ""
+
 	switch r {
 	case OneMinuteOf60Seconds:
-		return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, t.Location()), strconv.Itoa(t.Second())
+		ts = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, t.Location())
+		key = strconv.Itoa(t.Second())
 	case OneHourOf60Minutes:
-		return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location()), strconv.Itoa(t.Minute())
+		ts = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+		key = strconv.Itoa(t.Minute())
 	case OneDayOf24Hours:
-		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()), strconv.Itoa(t.Hour())
+		ts = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+		key = strconv.Itoa(t.Hour())
 	case OneMonthOfUpTo31Days:
-		return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location()), strconv.Itoa(t.Day())
+		ts = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
+		key = strconv.Itoa(t.Day())
 	case OneHourOf3600Seconds:
-		return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location()), strconv.Itoa(t.Minute()*60 + t.Second())
+		ts = time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+		key = strconv.Itoa(t.Minute()*60 + t.Second())
 	case OneDayOf1440Minutes:
-		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()), strconv.Itoa(t.Hour()*60 + t.Minute())
+		ts = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+		key = strconv.Itoa(t.Hour()*60 + t.Minute())
 	}
 
-	panic("invalid resolution")
+	return ts, key
 }
 
 // Join will return the timestamp of a single sample based on the start of a
@@ -72,42 +81,46 @@ func (r BasicResolution) Join(t time.Time, key string) time.Time {
 	// convert key to integer
 	i, _ := strconv.Atoi(key)
 
+	ts := time.Time{}
+
 	switch r {
 	case OneMinuteOf60Seconds:
-		return t.Add(time.Duration(i) * time.Second)
+		ts = t.Add(time.Duration(i) * time.Second)
 	case OneHourOf60Minutes:
-		return t.Add(time.Duration(i) * time.Minute)
+		ts = t.Add(time.Duration(i) * time.Minute)
 	case OneDayOf24Hours:
-		return t.Add(time.Duration(i) * time.Hour)
+		ts = t.Add(time.Duration(i) * time.Hour)
 	case OneMonthOfUpTo31Days:
-		return t.AddDate(0, 0, i-1)
+		ts = t.AddDate(0, 0, i-1)
 	case OneHourOf3600Seconds:
-		return t.Add(time.Duration(i) * time.Second)
+		ts = t.Add(time.Duration(i) * time.Second)
 	case OneDayOf1440Minutes:
-		return t.Add(time.Duration(i) * time.Minute)
+		ts = t.Add(time.Duration(i) * time.Minute)
 	}
 
-	panic("invalid resolution")
+	return ts
 }
 
 // SetSize will return the total amount of samples per set.
 func (r BasicResolution) SetSize() int {
+	size := 0
+
 	switch r {
 	case OneMinuteOf60Seconds:
-		return 60
+		size = 60
 	case OneHourOf60Minutes:
-		return 60
+		size = 60
 	case OneDayOf24Hours:
-		return 24
+		size = 24
 	case OneMonthOfUpTo31Days:
-		return 31
+		size = 31
 	case OneHourOf3600Seconds:
-		return 3600
+		size = 3600
 	case OneDayOf1440Minutes:
-		return 1440
+		size = 1440
 	}
 
-	panic("invalid resolution")
+	return size
 }
 
 // SetTimestamp will return the set timestamp for the given time.
